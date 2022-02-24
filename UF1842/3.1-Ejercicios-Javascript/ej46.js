cbfcd012ee08320040f42fcb852dbfcc1a9f3cd8
11ife <11ife>
2022-02-24 17:01:58 +0100

    Añadido ej 42-46

---
+++ UF1842/3.1-Ejercicios-Javascript/ej46.js
@@ -0,0 +1,8 @@
+/*
+Héctor Rodríguez Tovar
+24/02/2022
+Javascript-3.1
+*/
+/*
+46. Rellene un array con 5 números introducidos por teclado, luego copia los números de un array a otro array diferente multiplicados por 2 y muestre el segundo array
+*/
