ee85c90c5a637b81295ac60c8e1a7f92c923e948
11ife <11ife>
2022-02-24 18:47:35 +0100

    añadido ej 46-50

---
+++ UF1842/3.1-Ejercicios-Javascript/ej50.js
@@ -0,0 +1,14 @@
+/*
+Héctor Rodríguez Tovar
+24/02/2022
+Javascript-3.1
+*/
+/*
+50. Rellene un array con 10 números (introduce los 10 números por teclado) y luego busque un número concreto (introducido por pantalla).
+*/
+var num = [0];
+for(var i = 0; i < 10; i++){
+    num[i]=parseInt(prompt("dime un numero"));
+    console.log(num[i]);
+}
+
