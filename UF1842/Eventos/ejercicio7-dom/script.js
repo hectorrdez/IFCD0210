980cb933f0e71b1eeff2563800e91efe1de74379
11ife <11ife>
2022-03-10 20:12:50 +0100

    varios ejercicios añadidos

--- UF1842/Eventos/ejercicio7-dom/script.js
+++ UF1842/Eventos/ejercicio7-dom/script.js
@@ -1,3 +1,22 @@
-let consola = 0;
+document.getElementById("section").onload = consolaEscoge();
+document.getElementById("reset").onclick = consolaEscoge();
 
-function escogeConsola()
+
+function consolaEscoge(){
+    let consola = parseInt(Math.random()*3);
+    let jugador = prompt("Que escoges: piedra(p),papel(a) o tijeras(t)?");
+    let valor = 0;
+    if(jugador == "p") valor = 0;
+    if(jugador == "a") valor = 1;
+    if(jugador == "t") valor = 2;
+
+    console.log(`consola: ${consola} y jugador: ${valor}`);
+
+    if(valor > consola && ((consola != 0 && valor != 2)||(valor != 0 && consola != 2))){
+        alert("jugador a ganado")
+    }else if(valor == consola){
+        alert("Habeis empatado");
+    }else{
+        alert("la consola te ha ganado :<")
+    }
+}
---
+++ UF1842/Eventos/ejercicio9-dom/script.js
@@ -0,0 +1,48 @@
+let posicion = 0;
+let newpos = 0;
+let restantes = 0;
+let max = 64;
+
+let tirar = document.getElementById("tirar");
+tirar.onclick = posiciones;
+
+let reset = document.getElementById("reset");
+reset.onclick = resetMatch;
+
+let imagen = "<img src='ficha.png'>"
+let salida = document.getElementById("grid1");
+salida.innerHTML = imagen;
+
+function tirarDados(){
+    let tirada = parseInt(Math.random()*6+1);
+    console.log(tirada);
+    document.getElementById("dado").src = `dados/${tirada}.png`;
+    return tirada;
+}
+function movimientos(newpos){
+    for(let i = 1; i <= max; i++){
+        document.getElementById(`grid${i}`).innerText=i;
+    }
+    document.getElementById(`grid${newpos}`).innerHTML = imagen;
+    if(newpos == max){
+        alert("El juego se ha terminado");
+    }
+}
+function posiciones(){
+    let tirada = tirarDados();
+    if((posicion + tirada) > max){
+        restantes = (posicion + tirada) - max;
+        newpos = max - restantes;
+    }else{
+        newpos = posicion + tirada;
+    }
+    movimientos(newpos);
+    posicion = newpos;
+}
+function resetMatch(){
+    posicion = 1;
+    for(let i = 1; i <= max; i++){
+        document.getElementById(`grid${i}`).innerText=i;
+    }
+    document.getElementById(`grid${posicion}`).innerHTML = imagen;
+}
