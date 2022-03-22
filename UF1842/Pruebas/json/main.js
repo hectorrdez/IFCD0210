fc8ce6be3a67ea42bd1629ed9fadf68864a92fbc
11ife <11ife>
2022-03-22 20:46:49 +0100

    consultas a api y examen accesibilidad

--- UF1842/Pruebas/json/main.js
+++ UF1842/Pruebas/json/main.js
@@ -1,3 +1,30 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(data => console.log(data.title));
+
+
+let tabla = document.querySelector("table");
+
+let i = 0;
+setInterval(pedirDato, 100);
+
+function pedirDato() {
+  i++;
+  if (i < 100) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${i}`)
+      .then(response => response.json())
+      .then(data => guardar(data));
+  }
+}
+
+function guardar(data) {
+  let print = "";
+  print += "<tr>";
+  // data.userId
+  print += `<td>${data.userId}</td>`;
+  // data.id
+  print += `<td>${data.id}</td>`;
+  // data.title
+  print += `<td>${data.title}</td>`;
+  // data.completed
+  print += `<td>${data.completed}</td>`;
+  print += "</tr>";
+  tabla.innerHTML += print;
+}
