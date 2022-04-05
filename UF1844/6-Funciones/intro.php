c3712aaa85b0618244e9a9b3248d9293378a36c7
11ife <11ife>
2022-04-05 20:41:17 +0200

    grandes cambios en la calculador

---
+++ UF1844/6-Funciones/intro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function holamundo(){
+            echo "Hola mundo";
+        }
+    ?>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus odit quos dolorum voluptatibus unde voluptatem consectetur qui ipsum, sapiente eius esse? Neque dolor, aspernatur odio magnam sint officiis laudantium nulla.
+    Iste excepturi tenetur, esse laudantium architecto qui natus, nulla placeat consequuntur numquam similique ipsum facere? Quis, dolorum corporis aperiam quidem qui earum est, quam, animi fuga velit placeat sit quas.
+    Aliquid totam architecto quis ex soluta et neque iusto iste deserunt quidem commodi provident dolore praesentium id autem inventore reiciendis error magnam, eius corporis suscipit dicta eaque! Eaque, amet qui.
+    Quas tempore officia exercitationem veritatis totam, laborum nesciunt reprehenderit illum? Omnis facere atque quisquam culpa vel voluptatum porro aut dicta, repudiandae cum, aperiam quae, pariatur minus repellat voluptate numquam a!
+    Sed quisquam sunt reiciendis fugit, cupiditate pariatur facilis aliquid blanditiis impedit alias aliquam explicabo modi. Odio voluptate dolor, officiis debitis architecto ullam repellat, reiciendis quaerat, dolore aliquam in ad. Nobis?
+    Velit, sint tempore et porro minus, doloribus reiciendis distinctio quasi necessitatibus corrupti animi ad suscipit quos ex, nam at officiis omnis quis ducimus. Doloribus aspernatur impedit quidem ullam sed autem!
+    Temporibus voluptatibus voluptas at facilis autem excepturi et fugit incidunt impedit? Quod expedita magnam accusamus saepe vel ex reprehenderit eaque temporibus magni et enim, quasi sit laboriosam animi aliquam est.
+    Ipsa suscipit incidunt, voluptatum quidem totam laborum aut, rem quae maiores sit expedita eveniet autem quos cupiditate veniam a ullam nulla voluptates necessitatibus ratione, quia unde minima tenetur facilis! Et.
+    Asperiores ab repudiandae aliquid accusantium? Autem animi placeat quae suscipit praesentium quo voluptatem, obcaecati, ducimus nulla, sit qui et distinctio hic exercitationem amet velit quas illum dignissimos? Perspiciatis, enim repellendus.
+    Quam nihil asperiores saepe iure veritatis sit quod nemo tempore repellat, debitis odit error provident ea expedita praesentium minus distinctio, voluptatem non quas unde modi! Voluptate omnis impedit temporibus consectetur.
+
+    <br>
+    <br>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempora eum, reiciendis, exercitationem optio atque expedita fugit distinctio deserunt quas quidem enim eveniet accusantium nemo nulla corporis! Quisquam, incidunt doloribus.
+    Ea similique necessitatibus reprehenderit ipsum, voluptate quasi temporibus in quo pariatur laudantium reiciendis voluptas sed odio soluta quae dicta iste dolor omnis eius quod. Doloremque veritatis ipsum obcaecati ratione similique.
+    Eveniet ut rem itaque veritatis, sapiente ipsam officia cum consequuntur nemo earum alias. Minus reiciendis distinctio commodi debitis totam aspernatur accusantium, maiores quis in laborum possimus cupiditate, voluptatibus illum quae.
+    A atque, consectetur officiis sequi quaerat corrupti necessitatibus esse quibusdam nobis debitis earum harum tenetur fugiat libero voluptatibus eveniet accusamus rem aperiam! Ut assumenda, nostrum facilis pariatur odio fuga sunt.
+    Facilis ut quidem dignissimos earum architecto natus consectetur aliquid nam sit, similique non quasi cum provident dolor possimus placeat eius! Possimus illum fugiat nihil laudantium consequatur optio aut, rerum ducimus.
+    <br>
+    <br>
+    <?php
+        holamundo();
+    ?>
+</body>
+</html>
